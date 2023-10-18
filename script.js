94a2fd4046df8913908413bc739d71a534bb3c2e
Nischal lamichhane <108085352+hehenischal@users.noreply.github.com>
2023-10-18 10:48:20 +0545

    refactor: optimized the conversion algorithm

--- script.js
+++ script.js
@@ -10,48 +10,28 @@ document.addEventListener("DOMContentLoaded", function () {
         const fromUnit = fromUnitSelect.value;
         const toUnit = toUnitSelect.value;
 
-        let result;
-
+        // Convert all inputs to Kelvin
+        let temperatureInKelvin;
         if (fromUnit === "celsius") {
-            if (toUnit === "fahrenheit") {
-                result = (temperature * 9/5) + 32;
-            } else if (toUnit === "kelvin") {
-                result = temperature + 273.15;
-            } else if (toUnit === "rankine") {
-                result = (temperature + 273.15) * 9/5;
-            } else {
-                result = temperature;
-            }
+            temperatureInKelvin = temperature + 273.15;
         } else if (fromUnit === "fahrenheit") {
-            if (toUnit === "celsius") {
-                result = (temperature - 32) * 5/9;
-            } else if (toUnit === "kelvin") {
-                result = (temperature + 459.67) * 5/9;
-            } else if (toUnit === "rankine") {
-                result = temperature + 459.67;
-            } else {
-                result = temperature;
-            }
-        } else if (fromUnit === "kelvin") {
-            if (toUnit === "celsius") {
-                result = temperature - 273.15;
-            } else if (toUnit === "fahrenheit") {
-                result = (temperature * 9/5) - 459.67;
-            } else if (toUnit === "rankine") {
-                result = temperature * 9/5;
-            } else {
-                result = temperature;
-            }
+            temperatureInKelvin = (temperature + 459.67) * 5/9;
         } else if (fromUnit === "rankine") {
-            if (toUnit === "celsius") {
-                result = (temperature - 491.67) * 5/9;
-            } else if (toUnit === "fahrenheit") {
-                result = temperature - 459.67;
-            } else if (toUnit === "kelvin") {
-                result = (temperature * 5/9);
-            } else {
-                result = temperature;
-            }
+            temperatureInKelvin = temperature * 5/9;
+        } else {
+            temperatureInKelvin = temperature;
+        }
+
+        // Convert from Kelvin to the desired unit
+        let result;
+        if (toUnit === "celsius") {
+            result = temperatureInKelvin - 273.15;
+        } else if (toUnit === "fahrenheit") {
+            result = (temperatureInKelvin * 9/5) - 459.67;
+        } else if (toUnit === "rankine") {
+            result = temperatureInKelvin * 9/5;
+        } else {
+            result = temperatureInKelvin;
         }
 
         resultSpan.textContent = result.toFixed(2);
